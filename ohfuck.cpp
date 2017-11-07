52b0f1d42821f4252b81fd48ca7f7256f053c02c
Brian Lee <lkm1321@gmail.com>
2017-11-08 02:12:47 +1100

    Initial version

--- ohfuck.cpp
+++ ohfuck.cpp
@@ -7,36 +7,52 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp> 
 
-void imageCallback(const sensor_msgs::ImageConstPtr& depth_image){
-	SimpleBlobDetector:Params params; 
-	
-	// Min and max threshold.
-	params.minThreshold = 200; // mm 
-	params.maxThreshold = 2000; // mm
-	params.filterByArea = true; 
-	params.minArea = 250; // pixel^2
 
-	cv_bridge::CvImagePtr depth_image_cv = toCvCopy(depth);  
+class RosBlobDetector {
+	ros::NodeHandle _nh; // Node handle. 
+	image_transport::ImageTransport _it; // Needed for iamge transport.
+	image_transport::Subscriber _image_sub; // Subscription handle.  
+	image_transport::Publisher _image_pub;  // Publication handle. 
+	cv::SimpleBlobDetector _detector; 
+	vector<cv::Keypoints> keypoints; 
+public:
+	RosBlobDetector(): _it(_nh){
+		_image_sub = _it.subscribe("/camera/depth/image_raw", 10, &RosBlobDetector::imageCallback, this);
+		_image_pub = _it.advertise("/blob_detector/result", 10); 
+		cv::SimpleBlobDetector:Params params; 
+		// Min and max threshold.
+		params.minThreshold = 200; // mm 
+		params.maxThreshold = 2000; // mm
+		params.filterByArea = true; 
+		params.minArea = 250; // pixel^2
+		_detector = cv::SimpleBlobDetector(params); 
+	}
+	
+	void imageCallback(const sensor_msgs::ImageConstPtr& depth_image){
+
+		// Convert to OpenCV image
+		cv_bridge::CvImagePtr depth_image_cv_ptr; 
+
+		try{
+			cv_bridge::toCvCopy(depth, sensor_msgs::image_encodings::MONO16);  
+		} catch (cv_bridge::Exception& e){
+			ROS_ERROR("cv_bridge exception %s at %s, line %d", e.what(), __FILE__, __LINE__); 
+			return; 
+		}
+
+		_detector.detect(depth_image_cv_ptr->image, keypoints);
+		cv::drayKeypoints( depth_image_cv_ptr->image, depth_image_cv_ptr->image, cv::Scalar(0,0,255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+
+		_image_pub.publish(depth_image_cv_ptr->toImageMsg());
+	}
 
 }
-
 int main(int argc, char** argv){
 	
-	image_transport::ImageTransport it; 
-	image_transport::Subscriber image_sub; 
-	image_transport::Publisher image_pub; 
-	
 	// Initialize
-	ros::init(argc, argv, "camera_fcu_tf"); 
-	
-	// 60Hz update. 
-	ros::Rate r(60); 
-	
-	
-	while(ros::ok()){
-		
-		ros::spinOnce(); 
-		r.sleep(); 
-	}
+	ros::init(argc, argv, "ros_blob_detector"); 
+	RosBlobDetector rbd; 
+	// Process callbacks. 
+	ros::spin();
 	return 0;
 }
